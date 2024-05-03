fa0f3a6855a1831fdad00923847239a9dc96168d
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-03 18:30:20 +0900

    メイン画面のコンポーネントを作成

---
+++ vue-frontend/src/components/Bingo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="top">
+      <v-container
+        fluid
+        fill-height
+        class="d-flex align-center justify-center home_main_content"
+        style="height: 1000px"
+      >
+      </v-container>
+  </div>
+</template>
