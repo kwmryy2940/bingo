d979dd0b7c1be77e98031a783dfb26612cdaa926
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-03 21:46:41 +0900

    メイン画面のデザインの雛形を作成

--- vue-frontend/src/views/Home.vue
+++ vue-frontend/src/views/Home.vue
@@ -5,9 +5,6 @@ import Bingo from "../components/Bingo.vue";
 <template>
   <div class="home">
     <v-app>
-      <v-container fluid class="d-flex align-center justify-center">
-        <h1>Bingo</h1>
-      </v-container>
       <v-main>
         <Bingo />
       </v-main>
